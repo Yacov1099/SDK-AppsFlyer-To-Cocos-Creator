7917f2564fd22e4000848740594c98eabadc5c6b
Dekel Gilboa <dekel009@gmail.com>
2024-04-16 14:38:20 +0300

    Update AppsFlyerCocos.h

--- IOS/AppsFlyerCocos.h
+++ IOS/AppsFlyerCocos.h
@@ -5,12 +5,10 @@
 //  Created by Israel1 on 14/04/2024.
 //
 
-
 #import <AppsFlyerLib/AppsFlyerLib.h>
 #import "cocos.h"
 #include "platform/apple/JsbBridge.h"
 #import <Foundation/Foundation.h>
-
 
 #ifndef AppsFlyerCocos_h
 #define AppsFlyerCocos_h
@@ -19,8 +17,6 @@
 
 - (void)start;
 - (void)onConversionDataSuccess:(NSDictionary*)installData;
-
-
 
 #endif /* AppsFlyerCocos_h */
 @end
