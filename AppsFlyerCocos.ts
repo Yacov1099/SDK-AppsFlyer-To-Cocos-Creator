561e1b035c4eb4f1b78c4c4259a90bfba83789a8
Yacov1099 <104595753+Yacov1099@users.noreply.github.com>
2024-05-18 23:13:26 +0300

    Add files via upload

--- AppsFlyerCocos.ts
+++ AppsFlyerCocos.ts
@@ -1,20 +1,52 @@
-import { _decorator, native, sys } from 'cc';
+import { Component, _decorator, native, sys } from 'cc';
 
-type CallBack = (obj: Object, ...args: any[]) => any;
+interface CallBack {
+    (obj:Object, ...args:any[]):any 
+}
 
-export class AppsFlyerCocos{
+export const event_parameter = {
+    PRICE : "af_price",
+    CONTENT_ID:"af_content_id" 
+}
+
+export const events = {
+    LEVEL_ACHIEVED: "af_level_achieved",
+    PURCHASE : "af_purchase",
+    ADD_TO_CART: "af_add_to_cart"
+}
+
+type onNative = "startSDK" | "sentEvents"
+
+
+
+export class AppsFlyerCocos extends Component{
+
+    private content: Object | null
+
     
-    private sendToJs(content: string): string {
-        return content;
+    private convertJavaToObj(javaStr: String){
+        const keyValuePairs = javaStr.split(", ");
+            const content = {};
+            keyValuePairs.forEach(pair => {
+                const [key, value] = pair.split("=");
+                content[key?.trim()] = value?.trim();
+            });
+            return content
+    }
+    
+    private sendToNative(arg0: onNative, arg1: Object){
+        return new native.bridge.sendToNative(arg0, JSON.stringify(arg1));
     }
 
-    private setupIOSNativeBridge(callBack: CallBack): any {
+    private nativeBridge(callBack?: CallBack | null){
         try {
             native.bridge.onNative = (arg0: string, arg1: string): any => {
-                if (arg0 === 'sendToJs') {
-                    const str = this.sendToJs(arg1);
-                    const obj = JSON.parse(str);
-                    return callBack(obj)
+                const content = sys.OS.IOS?JSON.parse(arg1):this.convertJavaToObj(arg1);
+                if (arg0 === 'send_data_to_script') {
+                    if  (callBack) return callBack(content);
+                }
+                if (arg0 == 'end_event'){
+                    return console.log(content)
                 }
             };
         } catch (error) {
@@ -22,40 +54,42 @@ export class AppsFlyerCocos{
         }
     }
 
-    private setupAndroidNativeBridge(callBack: CallBack): any {
-        native.jsbBridgeWrapper.dispatchEventToNative("requestContent");
-        native.jsbBridgeWrapper.addNativeEventListener("sendToJs", (content: string) => {
-            const objStr = this.sendToJs(content);
-            // Converts the received string to proper JSON format
-            const keyValuePairs = objStr.split(", ");
-            const obj = {};
-            keyValuePairs.forEach(pair => {
-                const [key, value] = pair.split("=");
-                obj[key.trim()] = value.trim();
-            });
-            return callBack(obj)
-        });
-    }
+    ///////-SDK-/////////
 
-    public loadBridge(callBack: CallBack): any {
+    public  loadBridge(callBack?: CallBack | null): any{
         if (!sys.isNative) return;
-        if (sys.platform === sys.Platform.IOS) {
-            return this.setupIOSNativeBridge(callBack);
-           
-        } else if (sys.platform === sys.Platform.ANDROID) {
-            return this.setupAndroidNativeBridge(callBack);   
-        }        
+        if (sys.os === sys.OS.IOS){
+            native.reflection.callStaticMethod("AppsFlyerInit", "init");
+        }else if (sys.os === sys.OS.ANDROID){
+            native.reflection.callStaticMethod("AppsFlyerInit", "init","()V");
+        }
+        this.nativeBridge(callBack);      
     }
 
     public startSdk(devKey:String, appleId:String|null,isDebug=false,): void {
         if (!sys.isNative) return;
-        let obj = obj = {
-                    devKey,
-                    isDebug
-                 };
+        let config = {};
         if (sys.platform === sys.Platform.IOS) {
-            obj.appleId = appleId;
+            config = {
+                devKey,
+                isDebug,
+                appleId
+            };
+        } else if (sys.platform === sys.Platform.ANDROID) {
+            config = {
+                devKey,
+                isDebug
+            };
         }
-        native.bridge.sendToNative('startSDK', JSON.stringify(obj));
+        this.sendToNative( 'startSDK', config);
     }
+
+    public logEvents(event: String ,event_parameters: Array<Object>): void{
+        this.content =  {
+            event,
+            event_parameters
+        }
+        this.sendToNative('sentEvents', this.content);
+    }
+
 }
